874069d6c755f7302cf135f2969e1035f156eb55
Ricardo Zambon <ricardo.zambon@smartm.com>
2025-07-08 08:52:24 -0300

    feat: exports models from shared library

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -4,5 +4,6 @@ export * from './lib/configs-provider';
 export { ServicesHistoryViewComponent } from './lib/features/services-history';
 export * from './lib/helpers';
 export * from './lib/layouts';
+export * from './lib/models';
 export * from './lib/pipes';
 export * from './lib/services';
