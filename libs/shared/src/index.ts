7dd7ac9a3e2c995261cbc4dca484153664b99ecf
Ricardo Zambon <samuraizambon@outlook.com>
2026-05-01 18:51:54 -0300

    fix(shared): expose i18n assets

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -2,6 +2,7 @@ export * from './lib/auth';
 export * from './lib/components';
 export { OperationsHistoryChildListComponent, OperationsHistoryModalComponent } from './lib/features/operations-history';
 export { ServicesHistoryChildListComponent, ServicesHistoryViewComponent } from './lib/features/services-history';
+export * from './lib/i18n';
 export * from './lib/layouts';
 export * from './lib/models';
 export * from './lib/pipes';
